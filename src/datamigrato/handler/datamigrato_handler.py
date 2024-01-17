13333014d118ee81ee1701dbc664db7fb8de92a0
ritik dutta <ritikduttagd@gmail.com>
2024-01-17 12:45:49 +0530

    minor updates

--- src/datamigrato/handler/datamigrato_handler.py
+++ src/datamigrato/handler/datamigrato_handler.py
@@ -1,5 +1,3 @@
-# src/handler/datamigrato_handler.py
-# from datamigrato.migration.cassandra_migrator import CassandraCRUD
 from datamigrato.migration.mongodb_migrator import Mongo_migrator
 
 class Datamigrato:
@@ -9,4 +7,4 @@ class Datamigrato:
     def mongo_to_cassandra(self, primary_key='_id', flatten=False):
         to_cassandra = Mongo_migrator()
         to_cassandra.migrate_to_cassandra(primary_key)
-        print("done")
+        print("="*50)
