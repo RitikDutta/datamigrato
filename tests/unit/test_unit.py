7c1c90a544852d74fadb7c2af8414e5eae90e428
ritik dutta <ritikduttagd@gmail.com>
2024-01-17 17:53:48 +0530

    adding docker testpy

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -1,2 +1,12 @@
-def test_empty():
-    assert True
+import requests
+
+def test_api_response():
+    url = 'http://localhost:8080/api/v1/healthcheck'
+    response = requests.get(url)
+    
+    # Print the status code and the returned data for debugging
+    print(f"Status Code: {response.status_code}")
+    print(f"Response Content: {response.json()}")
+
+    # Assert that the status code is 200
+    assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
