1d83df725bc80e9d4886c9108da5dc5f2633249a
ritik dutta <ritikduttagd@gmail.com>
2024-01-18 17:26:59 +0530

    testing tests

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -2,8 +2,8 @@ import requests
 from unit_tests import test_api_response
 from datamigrato import test_py
 
-# def test_api():
-#     test_api_response.Test()
+def test_api():
+    test_api_response.Test()
 
 
 def test_module():
