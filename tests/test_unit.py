aeaca38f90f84259baa1344736ba578b51c22067
ritik dutta <ritikduttagd@gmail.com>
2024-01-23 12:14:31 +0530

    added api test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,10 +1,11 @@
 import requests
 from unit_tests import test_api_response
-from datamigrato import test_py
+from unit_tests import test_mongodb_migrator
 
 def test_api():
-    test_api_response.Test()
+    url = 'http://localhost:8080/api/v1/healthcheck'
+    # url = 'https://literate-guide-5v6q74x5vw5c4q44-8080.app.github.dev/api/v1/healthcheck'
+    test_api_response.Test(url)
 
-
-def test_module():
-    test_py.test_abc()
+def test_mongo():
+    test_mongodb_migrator.Test()
