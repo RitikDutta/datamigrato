8ab202bc3b1ea0d709b3be3a3ebb9f920f0e28c7
ritik dutta <ritikduttagd@gmail.com>
2024-01-23 12:11:50 +0530

    changed to modular code

--- src/datamigrato/handler/datamigrato_handler.py
+++ src/datamigrato/handler/datamigrato_handler.py
@@ -4,7 +4,11 @@ class Datamigrato:
     def __init__(self):
         print("Datamigrato class initialized")
 
-    def mongo_to_cassandra(self, primary_key='_id', flatten=False):
-        to_cassandra = Mongo_migrator()
-        to_cassandra.migrate_to_cassandra(primary_key)
+    def populate_mongo(self, url, client_url=None, database_name=None, collection_name=None):
+        migrator = Mongo_migrator(client_url=client_url, database_name=database_name, collection_name=collection_name)
+        migrator.populate_mongo(url)
+
+    def mongo_to_cassandra(self, primary_key='id', flatten=False, client_url=None, database_name=None, collection_name=None, keyspace_name=None, table_name=None, bundle=None, token=None):
+        migrator = Mongo_migrator(client_url=client_url, database_name=database_name, collection_name=collection_name)
+        migrator.migrate_to_cassandra(primary_key, flatten=flatten)
         print("="*50)
