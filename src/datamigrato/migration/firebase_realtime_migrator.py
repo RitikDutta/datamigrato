2dc020fb9798f4cace9f0d95db539a4921f79ec0
ritik dutta <ritikduttagd@gmail.com>
2024-02-10 14:20:47 +0530

    removed unnecessary codes

--- src/datamigrato/migration/firebase_realtime_migrator.py
+++ src/datamigrato/migration/firebase_realtime_migrator.py
@@ -21,6 +21,7 @@ class Firebase_realtime_migrator:
             self.firebase_realtime_adapter = FirebaseRealtimeDatabaseCRUD(refrence_url, root_node, token)
             
             self.firebase_realtime_data_list = self.firebase_realtime_adapter.read_all()
+            
         except Exception as e:
             print(e)
 
@@ -28,16 +29,10 @@ class Firebase_realtime_migrator:
         self.firebase_realtime_data_list = data = self.common_utils.get_users_freeAPI(url)
         self.firebase_realtime_adapter.insert_json_data(data)
         
-    def migrate_to_mongo(self, client_url=None, database_name=None, collection_name=None):
-
-        
+    def migrate_to_mongo(self, client_url=None, database_name=None, collection_name=None):        
         mongo_adapter = MongoDB_CRUD(client_url=client_url, database_name=database_name, collection_name=collection_name)
         mongo_adapter.create_many(data_list=self.firebase_realtime_data_list)        
 
     def migrate_to_cassandra(self, primary_key, keyspace_name=None, table_name=None, bundle=None, token=None, flatten=False):
-        keyspace_name = keyspace_name or "data_migrato_key"
-        table_name = table_name or "new_table"
-
-        #cassandra object
         cassandra_adapter = CassandraCRUD(keyspace_name, table_name, bundle, token)
         cassandra_adapter.insert_json_data(data = self.firebase_realtime_data_list, primary_key=primary_key, flatten=flatten)
--- src/datamigrato/migration/mongodb_migrator.py
+++ src/datamigrato/migration/mongodb_migrator.py
@@ -1,6 +1,7 @@
 from datamigrato.utils.common_utils import Common_utils
 from datamigrato.adapters.mongodb_adapter import MongoDB_CRUD
 from datamigrato.adapters.cassandra_adapter import CassandraCRUD
+from datamigrato.adapters.firebase_realtime_adapter import FirebaseRealtimeDatabaseCRUD
 
 class Mongo_migrator:
     def __init__(self, parameter_file=None, client_url=None, database_name=None, collection_name=None):
@@ -30,20 +31,11 @@ class Mongo_migrator:
         self.mongo_data_list = data = self.common_utils.get_users_freeAPI(url)
         self.mongo_adapter.create_many(data)
 
-
     def migrate_to_cassandra(self, primary_key, keyspace_name=None, table_name=None, bundle=None, token=None, flatten=False):
-        keyspace_name = keyspace_name or "data_migrato_key"
-        table_name = table_name or "new_table"
-
-        #cassandra object
-        if self.mongo_adapter.is_connected:
-            cassandra_adapter = CassandraCRUD(keyspace_name, table_name, bundle, token)
-        else:
-            print("Skipping cassandra connection")
-
+        cassandra_adapter = CassandraCRUD(keyspace_name, table_name, bundle, token)
         cassandra_adapter.insert_json_data(data = self.mongo_data_list, primary_key=primary_key, flatten=flatten)
 
-    def migrate_to_firebase(self):
+    def migrate_to_firebase_realtime(self, refrence_url, root_node, token=None):
         pass
 
     def migrate_to_firestore(self):
