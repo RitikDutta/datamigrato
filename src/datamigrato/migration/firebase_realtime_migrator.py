0eff4ca056ee4a969e520ef0f4ab999ef582754a
ritik dutta <ritikduttagd@gmail.com>
2024-02-12 23:40:47 +0530

    params are required

--- src/datamigrato/migration/firebase_realtime_migrator.py
+++ src/datamigrato/migration/firebase_realtime_migrator.py
@@ -4,24 +4,11 @@ from datamigrato.adapters.cassandra_adapter import CassandraCRUD
 from datamigrato.adapters.firebase_realtime_adapter import FirebaseRealtimeDatabaseCRUD
 
 class Firebase_realtime_migrator:
-    def __init__(self, parameter_file=None, refrence_url=None, root_node=None, token=None):
+    def __init__(self, refrence_url, root_node, token=None):
         self.common_utils = Common_utils()
         try:
-            refrence_url = refrence_url
-            root_node = root_node
-
-            if not all([refrence_url, root_node]):
-                parameters = self.common_utils.read_parameters(parameter_file)
-                refrence_url = parameters.get('refrence_url')
-                root_node = parameters.get('root_node')
-            
-            if not all([refrence_url, root_node]):
-                raise ValueError("Missing required database parameters")
-
             self.firebase_realtime_adapter = FirebaseRealtimeDatabaseCRUD(refrence_url, root_node, token)
-            
             self.firebase_realtime_data_list = self.firebase_realtime_adapter.read_all()
-            
         except Exception as e:
             print(e)
 
@@ -33,6 +20,6 @@ class Firebase_realtime_migrator:
         mongo_adapter = MongoDB_CRUD(client_url=client_url, database_name=database_name, collection_name=collection_name)
         mongo_adapter.create_many(data_list=self.firebase_realtime_data_list)        
 
-    def migrate_to_cassandra(self, primary_key, keyspace_name=None, table_name=None, bundle=None, token=None, flatten=False):
-        cassandra_adapter = CassandraCRUD(keyspace_name, table_name, bundle, token)
+    def migrate_to_cassandra(self, primary_key, keyspace_name=None, table_name=None, secure_bundle=None, token=None, flatten=False):
+        cassandra_adapter = CassandraCRUD(keyspace_name=keyspace_name, table_name=table_name, secure_bundle=secure_bundle, token=token)
         cassandra_adapter.insert_json_data(data = self.firebase_realtime_data_list, primary_key=primary_key, flatten=flatten)
