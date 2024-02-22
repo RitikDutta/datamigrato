d3a910af750d17f3a09982a954a1d4ff04c5cab1
ritik dutta <ritikduttagd@gmail.com>
2024-02-22 17:08:08 +0530

    fixed test case

--- tests/unit_tests/test_mongodb_migrator.py
+++ tests/unit_tests/test_mongodb_migrator.py
@@ -20,7 +20,7 @@ class Test:
 
 		self.handler = Datamigrato()
 		self.populate_mongo(url, database_name, collection_name, client_url, cred_file)
-		self.mongo_to_cassandra(database_name=self.database_name, collection_name=self.collection_name, client_url=self.client_url, cred_file=self.cred_file, keyspace_name=self.keyspace_name, table_name=self.table_name, primary_key=self.primary_key, secure_bundle=self.secure_bundle, token=self.token, flatten=flatten)
+		self.mongo_to_cassandra(database_name=self.database_name, collection_name=self.collection_name, client_url=self.client_url, cred_file=self.cred_file, keyspace_name=self.keyspace_name, table_name=self.table_name, primary_key=self.primary_key, secure_bundle=self.secure_bundle, cassandra_token=self.token, flatten=flatten)
 
 	def getcreds(self):
 		creds = 'creds'
@@ -30,6 +30,6 @@ class Test:
 		self.handler.populate_mongo(url=url, client_url=client_url, database_name=database_name, collection_name=collection_name, cred_file=cred_file)
 		assert True
 	
-	def mongo_to_cassandra(self, database_name, collection_name, keyspace_name, table_name, primary_key='id', flatten=False, client_url=None, cred_file=None, secure_bundle=None, token=None):
-		self.handler.mongo_to_cassandra(primary_key=primary_key, client_url=self.client_url, cred_file=cred_file, database_name=self.database_name, collection_name=self.collection_name, keyspace_name=keyspace_name, table_name=table_name, secure_bundle=secure_bundle, token=token, flatten=flatten)
+	def mongo_to_cassandra(self, database_name, collection_name, keyspace_name, table_name, primary_key='id', flatten=False, client_url=None, cred_file=None, secure_bundle=None, cassandra_token=None):
+		self.handler.mongo_to_cassandra(primary_key=primary_key, client_url=self.client_url, cred_file=cred_file, database_name=self.database_name, collection_name=self.collection_name, keyspace_name=keyspace_name, table_name=table_name, secure_bundle=secure_bundle, token=cassandra_token, flatten=flatten)
 		assert True
