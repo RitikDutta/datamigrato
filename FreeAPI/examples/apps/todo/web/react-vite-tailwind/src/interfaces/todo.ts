11b3de439adfcb8f2e2c89b1fd48720506b7b902
ritik dutta <ritikduttagd@gmail.com>
2024-02-24 01:08:03 +0530

    removed locak forked freeapi

--- FreeAPI/examples/apps/todo/web/react-vite-tailwind/src/interfaces/todo.ts
+++
@@ -1,8 +0,0 @@
-export interface TodoInterface {
-  title: string;
-  description: string;
-  isComplete: boolean;
-  createdAt: string;
-  updatedAt: string;
-  _id: string;
-}
--- FreeAPI/src/validators/apps/ecommerce/category.validators.js
+++
@@ -1,9 +0,0 @@
-import { body, param } from "express-validator";
-
-const categoryRequestBodyValidator = () => {
-  return [
-    body("name").trim().notEmpty().withMessage("Category name is required"),
-  ];
-};
-
-export { categoryRequestBodyValidator };
