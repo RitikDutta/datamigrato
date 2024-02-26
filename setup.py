eae9e0f452e0e84b8c5436869605e3073dd531ff
ritik dutta <ritikduttagd@gmail.com>
2024-02-26 16:28:34 +0530

    added pypi readme and version changed

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from setuptools import setup, find_packages
 
-with open("./README_PYPI.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
-
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 REPO_NAME = "datamigrato"
 PKG_NAME= "datamigrato"
 AUTHOR_USER_NAME = "ritikdutta"
@@ -17,7 +14,7 @@ setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description='A Python package for migrating data between various databases.',
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url=f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}',
     project_urls={
